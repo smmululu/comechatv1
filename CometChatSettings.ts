486feadbfb0b2bd2e9d1cbf7fe244636a724a3a2
smmululu <sautiea@gmail.com>
2025-08-19 13:16:24 +0300

    Add files via upload

---
+++ CometChatSettings.ts
@@ -0,0 +1,100 @@
+
+        export const CometChatSettings = {
+  "chatFeatures": {
+    "coreMessagingExperience": {
+      "typingIndicator": false,
+      "threadConversationAndReplies": false,
+      "photosSharing": true,
+      "videoSharing": true,
+      "audioSharing": true,
+      "fileSharing": false,
+      "editMessage": false,
+      "deleteMessage": false,
+      "messageDeliveryAndReadReceipts": true,
+      "userAndFriendsPresence": true
+    },
+    "deeperUserEngagement": {
+      "mentions": false,
+      "reactions": true,
+      "messageTranslation": true,
+      "polls": false,
+      "collaborativeWhiteboard": false,
+      "collaborativeDocument": false,
+      "voiceNotes": true,
+      "emojis": true,
+      "stickers": true,
+      "userInfo": true,
+      "groupInfo": true
+    },
+    "aiUserCopilot": {
+      "conversationStarter": false,
+      "conversationSummary": false,
+      "smartReply": false
+    },
+    "groupManagement": {
+      "createGroup": true,
+      "addMembersToGroups": true,
+      "joinLeaveGroup": true,
+      "deleteGroup": true,
+      "viewGroupMembers": true
+    },
+    "moderatorControls": {
+      "kickUsers": true,
+      "banUsers": true,
+      "promoteDemoteMembers": true
+    },
+    "privateMessagingWithinGroups": {
+      "sendPrivateMessageToGroupMembers": true
+    }
+  },
+  "callFeatures": {
+    "voiceAndVideoCalling": {
+      "oneOnOneVoiceCalling": true,
+      "oneOnOneVideoCalling": true,
+      "groupVideoConference": true,
+      "groupVoiceConference": true
+    }
+  },
+  "layout": {
+    "withSideBar": false,
+    "tabs": [
+      "chats",
+      "calls",
+      "users",
+      "groups"
+    ],
+    "chatType": "user"
+  },
+  "style": {
+    "theme": "light",
+    "color": {
+      "brandColor": "#838282",
+      "primaryTextLight": "#1b1b1b",
+      "primaryTextDark": "#FFFFFF",
+      "secondaryTextLight": "#727272",
+      "secondaryTextDark": "#989898"
+    },
+    "typography": {
+      "font": "roboto",
+      "size": "default"
+    }
+  },
+  "noCode": {
+    "docked": false,
+    "styles": {
+      "buttonBackGround": "#141414",
+      "buttonShape": "rounded",
+      "openIcon": "https://cdn.jsdelivr.net/npm/@cometchat/chat-embed@latest/dist/icons/docked_open_icon.svg",
+      "closeIcon": "https://cdn.jsdelivr.net/npm/@cometchat/chat-embed@latest/dist/icons/docked_close_icon.svg",
+      "customJs": "",
+      "customCss": ""
+    }
+  },
+  "agent": {
+    "chatHistory": true,
+    "newChat": true,
+    "agentIcon": "",
+    "showAgentIcon": true
+  }
+};
+      
